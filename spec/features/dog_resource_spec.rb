46237a209382ee6ee73cdc46b02f2f811c1d7714
Anna Sobiepanek <asobiepanek@zetaglobal.com>
2021-05-01 20:55:29 -0400

    Add support for multi-file upload

--- spec/features/dog_resource_spec.rb
+++ spec/features/dog_resource_spec.rb
@@ -9,9 +9,21 @@ describe 'Dog resource', type: :feature do
     visit new_dog_path
     fill_in 'Name', with: 'Speck'
     fill_in 'Description', with: 'Just a dog'
-    attach_file 'Image', 'spec/fixtures/images/speck.jpg'
+    attach_file 'dog_image', 'spec/fixtures/images/speck.jpg'
     click_button 'Create Dog'
     expect(Dog.count).to eq(1)
+  end
+
+  it 'can create a profile with multiple images' do
+    sign_in user
+    visit new_dog_path
+    fill_in 'Name', with: 'Speck Two'
+    fill_in 'Description', with: 'Just a dog with two images'
+    attach_file 'dog_image', ['spec/fixtures/images/speck.jpg','spec/fixtures/images/bijou.jpg']
+    click_button 'Create Dog'
+    expect(Dog.count).to eq(1)
+    dob = Dog.find_by_name("Speck Two")
+    expect(dob.images.count).to eq(2)
   end
 
   it 'can edit a dog profile' do
